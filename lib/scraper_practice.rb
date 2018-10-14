0783ffa089268205a029e4ff430a0ae6dcd84336
Drew Bragg <DRBragg@gmail.com>
2018-10-14 15:34:04 -0400

    thangs

--- lib/scraper_practice.rb
+++ lib/scraper_practice.rb
@@ -2,40 +2,29 @@ require 'nokogiri'
 require 'open-uri'
 require 'pry'
 
-
 def scrape
   beer_info = []
 
-  site = Nokogiri::HTML(open("https://www.craftbeer.com/beer/beer-styles-guide"))
-  
-  site.css("#content .style").each do |beer_site|
+  site = Nokogiri::HTML(open('https://www.craftbeer.com/beer/beer-styles-guide'))
+
+  site.css('#content .style').each do |beer_site|
     each_beer = {
-      :style_family => beer_site.css(".family-name").text,
-      :style_name => beer_site.css(".style-name").text,
-      :style_description => beer_site.css("p")[1].text
-      # :commercial_examples => {
-      #   # nested hash containing brewery and beer name
-      # }
+      style_family: beer_site.css('.family-name').text,
+      style_name: beer_site.css('.style-name').text,
+      style_description: beer_site.css('p')[1].text,
+      commercial_beers: []
     }
+    beer_site.css('.winners li').each do |us_beer|
+      commercial_beer = {
+        brewery: us_beer.css('.value').text,
+        beer_name: us_beer.css('.brewery').text
+      }
+      each_beer[:commercial_beers] << commercial_beer
+    end
 
-
-
-  beer_info << each_beer
+    beer_info << each_beer
   end
-
-  
   binding.pry
-  site.css("#content .style").css(".winners .value").first.text
-
 end
 
 scrape
-
-    # beer_site.css(".winners").each do |us_beer|
-    #   commercial_beers = {
-    #     :brewery => us_beer.css(".value").text,
-    #     :beer_name => us_beer.css(".brewery").text
-    #   }
-    # end
-
-    # Test
